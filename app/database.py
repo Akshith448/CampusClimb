c4fb5c01afe964262264ff5b20e1f6d57e29f642
Rahul Kumar <rahulkumar05au@gmail.com>
2026-09-08 22:52:17 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
  
-load_dotenv()
+load_dotenv() 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError(
